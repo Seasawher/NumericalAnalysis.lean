fa5eb00fc51b49ae4c373c50cc49ba021a125ca7
Seasawher <tellagoodstory@icloud.com>
2024-08-23 18:15:12 +0900

    最初のコミット

---
+++ README.md
@@ -0,0 +1,3 @@
+# NumericalAnalysis
+
+朝倉書店「Python による数値計算入門」河村、桑名著のノートです。
---
+++ lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package "NumericalAnalysis" where
+  -- add package configuration options here
+
+lean_lib «NumericalAnalysis» where
+  -- add library configuration options here
+
