f12626a23cc48e771b7e305c6101ab3f3fa53178
Seasawher <tellagoodstory@icloud.com>
2024-08-23 18:20:15 +0900

    lakefile 更新

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,13 @@ open Lake DSL
 
 package "NumericalAnalysis" where
   -- add package configuration options here
+  leanOptions := #[
+    ⟨`autoImplicit, false⟩,
+    ⟨`relaxedAutoImplicit, false⟩,
+    ⟨`linter.missingDocs, true⟩
+  ]
 
+@[default_target]
 lean_lib «NumericalAnalysis» where
   -- add library configuration options here
-
+  globs := #[.submodules `NumericalAnalysis]
